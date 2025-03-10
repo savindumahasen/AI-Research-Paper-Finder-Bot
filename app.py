551abe9f47177ad2b168630c7b845bec92e7c406
Savindu <savinduruhunuhewa@gmail.com>
2025-03-10 16:27:21 +0530

    Google search agent

--- app.py
+++ app.py
@@ -15,21 +15,20 @@ if not GOOGLE_API_KEY:
 else:
     genai.configure(api_key=GOOGLE_API_KEY)
 
-
-#agent = Agent(
-#    tools=[GoogleSearch()],
-#    model=Gemini(id="gemini-2.0-flash-exp"),
-#    description="You are a research agent that helps users find the latest research papers.",
-#    instructions=[
-#        "Given a topic by the user, respond should be 10 latest research papers about that topic.",
-#        "give the research papers abstractions,with acual links.",
-#        "Search in English and French",
-#    ],
-#    show_tool_calls=False,
-#    markdown=True,
-#    debug_mode=False,
-#)
-#agent.print_response("How cyber attecks affected on banking sector", markdown=True)
+google_search_agent = Agent(
+    tools=[GoogleSearch()],
+    model=Gemini(id="gemini-2.0-flash-exp"),
+    description="You are a research agent that helps users find the latest research papers.",
+    instructions=[
+        "Given a topic by the user, respond should be 10 latest research papers about that topic.",
+        "give the research papers abstractions,with acual links.",
+        "Search in English and French",
+    ],
+    show_tool_calls=False,
+    markdown=True,
+    debug_mode=False,
+)
+agent.print_response("How cyber attecks affected on banking sector", markdown=True)
 
 
 
