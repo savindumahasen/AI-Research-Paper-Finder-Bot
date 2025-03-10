435bb816549e19767a22e8615fea8985395098a3
Savindu <savinduruhunuhewa@gmail.com>
2025-03-10 17:00:20 +0530

    Create the multi_ai_agent

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from phi.agent  import Agent
-from phi.model.google import Gemini
+from phi.model.groq import Groq
 from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.googlesearch import GoogleSearch
 import google.generativeai as genai
@@ -7,16 +7,17 @@ from dotenv  import load_dotenv
 load_dotenv()
 import os
 
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 
 
-if not GOOGLE_API_KEY:
-    raise ValueError("GOOGLEAPIKEY is missing. Please check your .env file")
-else:
-    genai.configure(api_key=GOOGLE_API_KEY)
+#if not GOOGLE_API_KEY:
+ #   raise ValueError("GOOGLEAPIKEY is missing. Please check your .env file")
+#else:
+ #   genai.configure(api_key=GOOGLE_API_KEY)
 
 web_search_agent=Agent(
     tools=[DuckDuckGo()],
+    model=Groq(id="Deepseek-R1-Distill-Llama-70b"),
     description="You are a web search agent that helps users find the latest research papers.",
     instructions=[
         "Given a topic by the user, respond should be 10 latest research papers about that topic.",
@@ -31,7 +32,7 @@ web_search_agent=Agent(
 
 google_search_agent = Agent(
     tools=[GoogleSearch()],
-    model=Gemini(id="gemini-2.0-flash-exp"),
+    model=Groq(id="Deepseek-R1-Distill-Llama-70b"),
     description="You are a research agent that helps users find the latest research papers.",
     instructions=[
         "Given a topic by the user, respond should be 10 latest research papers about that topic.",
@@ -44,20 +45,20 @@ google_search_agent = Agent(
 )
 
 
-#multi_ai_agent =Agent(
-#    team=[google_search_agent,web_search_agent],
-#    model=Gemini(id="gemini-2.0-flash-exp"),
-#    description="You are a research agent that helps users find the latest research papers.",
-#    instructions=[
-#        "Given a topic by the user, respond should be 10 latest research papers about that topic.",
-#        "give the research papers abstractions,with acual links.",
-#        "Search in English and French",
-#    ],
-#    show_tool_calls=False,
-#    markdown=True,
-#    debug_mode=False,
-#)
-#multi_ai_agent.print_response("How cyber attecks affected on banking sector", markdown=True)
+multi_ai_agent =Agent(
+    team=[google_search_agent,web_search_agent],
+    model=Groq(id="Deepseek-R1-Distill-Llama-70b"),
+    description="You are a research agent that helps users find the latest research papers.",
+    instructions=[
+        "Given a topic by the user, respond should be 10 latest research papers about that topic.",
+        "give the research papers abstractions,with acual research links names.",
+        "Search in English and French",
+    ],
+    show_tool_calls=False,
+    markdown=True,
+    debug_mode=False,
+)
+multi_ai_agent.print_response("How cyber attecks affected on banking sector", markdown=True)
 
 
 
